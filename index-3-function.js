bb98e0d6ce0665d043390faeb881b66d0e7e7305
Sergii Bartytskyi <s.bartycjkyj@gmail.com>
2022-10-23 23:42:03 +0300

    update function hw

--- index-3-function.js
+++ index-3-function.js
@@ -973,3 +973,174 @@
 // // true
 // includes(["apple", "plum", "pear", "orange"], "kiwi");
 // // false
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+  // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+
+//     for (const potion of this.potions) {
+      
+//       if (potion.name === newPotion.name) {
+//       return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       }
+//     }
+    
+//     this.potions.push(newPotion);
+//   },
+  
+//   removePotion(potionName) {
+// for (const potion of this.potions) {
+// //   const potionNameIndex = this.potions[potion[potionName]];
+// //         console.table(potionNameIndex);
+        
+//         if (this.potions[potion.name] === potionName) {
+//                 this.potions.splice(this.potions[potion]);
+                
+//         }
+//         console.table(this.potions);
+// }
+    
+    // for (let i = 0; i < this.potions.length; i += 1) {
+
+    //   if (potionName === this.potions[i].name) {
+    //     this.potions.splice(i, 1);
+    //   }
+    // }
+
+//     return `Potion ${potionName} is not in inventory!`;
+// },
+//   updatePotionName(oldName, newName) {
+
+//     for (const potion of this.potions) {
+      
+//       if (potion.name === oldName) {
+//         potion.name = newName;
+//       }
+        
+//    }
+//     return `Potion ${oldName} is not in inventory!`;
+//   },
+  // Change code above this line
+// };
+
+// atTheOldToad.getPotions();
+
+// atTheOldToad.addPotion({ name: "Invisibility", price: 620 });
+
+// atTheOldToad.addPotion({ name: "Power potion", price: 270 });
+
+// atTheOldToad.addPotion({ name: "Dragon breath", price: 700 });
+
+// atTheOldToad.addPotion({ name: "Stone skin", price: 240 });
+
+// atTheOldToad.removePotion("Dragon breath");
+
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth")
+
+// atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion");
+// ==========================================================================
+
+// reduce
+// ===================================================================
+// const solary = {
+//   mango: 100,
+//   pol: 50,
+//   ajax:150,
+// }
+
+// const totalSolary = Object.values(solary).reduce((total, value) => total + value, 0);
+// console.log(totalSolary);
+// ======================================================================
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39
+//   }
+// ]
+// Change code below this line
+// const getFriends = (users) => {
+//   const allFriend = this.users.flatMap(user => user.friends);
+//   console.log(allFriend);
+//   const uniqueFriends = allFriend.filter((user, index, array) =>
+//     array.IndexOf(user) === index);
+   
+// };
+// Change code above this line
+// console.log(getFriends(uniqueFriends));
+//["Sharron Pace", "Briana Decker", "Marilyn Mcintosh", 
+//"Padilla Garrison", "Naomi Buckner", "Goldie Gentry", "Aisha Tran",
+//"Jordan Sampson", "Eddie Strong", "Jacklyn Lucas", "Linda Chapman"]
