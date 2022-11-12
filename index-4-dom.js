9daebede9b39ed2ebf8b04242a906b13048686ee
SergiiBartytskyi <108729124+SergiiBartytskyi@users.noreply.github.com>
2022-11-13 00:01:18 +0200

    add index-4 -dom

---
+++ index-4-dom.js
@@ -0,0 +1,37 @@
+// const listWithId = document.querySelector("#menu");
+
+// const helloPos = listWithId.firstElementChild;
+// const helloText = helloPos.innerHTML;
+// helloPos.innerHTML = `<li>${helloText} <span class = red>World</span></li>`;
+// // listWithId.style.textTransform = "uppercase";
+// // listWithId.style.fontSize = "26px";
+// // listWithId.style.color = "blue";
+// console.log(listWithId);
+// console.log(helloPos);
+// console.log(helloText);
+// console.log(helloPos.innerHTML);
+
+// const listWithClass = document.querySelector(".menu");
+// listWithClass.style.textTransform = "uppercase";
+// listWithClass.style.fontSize = "26px";
+// listWithClass.style.color = "blue";
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = "tomato";
+// console.log(firstMenuItem);
+
+const firstTextEl = document.querySelector(".menu-item");
+console.log(firstTextEl);
+
+const newEl = document.createElement("div");
+newEl.innerHTML = `Живи <span class = "red"> як хочеш </span>!`;
+console.log(newEl.innerHTML);
+
+firstTextEl.before(newEl);
